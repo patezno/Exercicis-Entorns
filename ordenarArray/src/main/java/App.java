32e95c48e1e69d78f23610015be7f0b3dcd0812d
unknown <patezno97@gmail.com>
2019-02-18 20:04:59 +0100

    ejercicio ordenar array hecho

--- ordenarArray/src/main/java/App.java
+++ ordenarArray/src/main/java/App.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+
 public class App {
 
     public static void main(String[] args) {
 
+        boolean intercambio = true;
+        int[] array = {4, 7, 12, 65, 23, 767, 32, 1, 3};
+        int pos = 0;
+        int aux = 0;
+
+        while (intercambio) {
+            intercambio = false;
+            pos = 0;
+            while (pos < array.length -1) {
+                if (array[pos] < array[pos + 1]) {
+                    aux = array[pos];
+                    array[pos] = array[pos + 1];
+                    array[pos + 1] = aux;
+                    intercambio = true;
+                }
+                pos += 1;
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 
 }
